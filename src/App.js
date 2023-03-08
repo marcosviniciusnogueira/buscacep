4d300527faf35d2c9fd20d0c06c43e9d2a2802fc
Marcos Vinicius Nogueira <kimnog@gmail.com>
2023-03-08 15:03:13 -0300

    Busca CEP

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import { useForm } from 'react-hook-form';
 
 function App() {
+
+  const {register, handleSubmit, setValue} = useForm();
+
+  const onSubmit = (e) => {
+    console.log(e);
+  }
+
+  const checaCEP = (e) => {
+    const cep = e.target.value.replace(/\D/g, '');
+    fetch(`https://viacep.com.br/ws/${cep}/json/`).then(res => res.json()).then(data => {
+      setValue('rua', data.logradouro);
+      setValue('bairro', data.bairro);
+      setValue('cidade', data.localidade);
+      setValue('estado', data.uf);
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <h1>Busca Cep:</h1>
+      <label>
+        CEP:
+        <input type="text" {...register("cep")} onBlur={checaCEP}/>
+      </label>
+      <label>
+        Rua:
+        <input type="text" {...register("rua")}/>
+      </label>
+      <label>
+        Bairro:
+        <input type="text" {...register("bairro")}/>
+      </label>
+      <label>
+        Cidade:
+        <input type="text" {...register("cidade")}/>
+      </label>
+      <label>
+        Estado:
+        <input type="text" {...register("estado")}/>
+      </label>
+      <button type="submit">Enviar</button>
+    </form>
   );
 }
 
